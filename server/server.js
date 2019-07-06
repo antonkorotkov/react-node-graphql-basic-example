4fcc8775140367515ea15ae7123f6ecb08d4e532
Anton <antonkorotkoff@gmail.com>
2019-07-06 13:14:55 +0300

    complete

--- server/server.js
+++ server/server.js
@@ -1,18 +1,40 @@
+const { ApolloServer, gql } = require('apollo-server-express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const expressJwt = require('express-jwt');
+const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const db = require('./db');
 
 const port = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
+const typeDefs = gql( fs.readFileSync('./schema.graphql', {encoding: 'utf-8'}) );
+const resolvers = require('./resolvers');
+
 const app = express();
 app.use(cors(), bodyParser.json(), expressJwt({
   secret: jwtSecret,
   credentialsRequired: false
 }));
+
+const graphqlServer = new ApolloServer({
+  typeDefs, 
+  resolvers,
+  context: ({req}) => {
+    if (!req.user) throw new AuthorizationError('you must be logged in to query this schema');
+    return {user: req.user && db.users.get(req.user.sub)}
+  }
+});
+graphqlServer.applyMiddleware({app});
+
+// const schema = makeExecutableSchema({typeDefs, resolvers});
+// app.use('/graphql', graphqlExpress(req => ({
+//   schema,
+//   context: {user: req.user && db.users.get(req.user.sub)}
+// })));
+// app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
 
 app.post('/login', (req, res) => {
   const {email, password} = req.body;
